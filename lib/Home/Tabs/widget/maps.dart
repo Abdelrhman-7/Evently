aa763a588ca789ca10659e3358ec4573c153ce77
abdelrhman <wwwabdelrhmanmahmoud22@gmail.com>
2025-11-23 22:53:01 +0200

    creat edit&& event_details_screen.dart

---
+++ lib/Home/Tabs/widget/maps.dart
@@ -0,0 +1,14 @@
+import 'package:evently/core/colormanager.dart';
+import 'package:flutter/material.dart';
+
+class Maps extends StatelessWidget {
+  const Maps({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(backgroundColor: Colormanager.black),
+      body: Container(color: Colormanager.white),
+    );
+  }
+}
